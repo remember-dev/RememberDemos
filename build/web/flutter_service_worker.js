253296a2341622e9bb6a6f63bab7e9431bea3fe0
Jonathan de Gaston <developerjon5@gmail.com>
2025-03-03 11:27:32 -0700

    fresh build for web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "8ddecb1ff0347da295a87877125e5d1e
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "29f8b0612db237adf98fd4da90f24815",
+"flutter_bootstrap.js": "9781fe85e0b7598d0749e71a2a7c2358",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
